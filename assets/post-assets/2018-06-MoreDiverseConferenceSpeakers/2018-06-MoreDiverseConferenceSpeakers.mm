b82b649bc4bcccb73834d108c9233fce9b89d089
Sean Killeen <SeanKilleen@gmail.com>
2018-06-04 21:10:17 -0400

    add mind map files

---
+++ assets/post-assets/2018-06-MoreDiverseConferenceSpeakers/2018-06-MoreDiverseConferenceSpeakers.mm
@@ -0,0 +1,270 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1528156442399" ID="ID_1916760956" MODIFIED="1528160514853" TEXT="Goal: Increase underrepresented conference speakers">
+<edge COLOR="#000000" WIDTH="8"/>
+<node COLOR="#0033ff" CREATED="1528156581549" ID="ID_355124698" MODIFIED="1528160514855" POSITION="right" TEXT="People who feel unsafe at conferences">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528156765313" ID="ID_1137521043" MODIFIED="1528160514857" TEXT="Submit talks">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528156844447" ID="ID_1601344470" MODIFIED="1528160514859" TEXT="Clear COC">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156847562" ID="ID_805648134" MODIFIED="1528160514860" TEXT="Conf staff trained on COC">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156854133" ID="ID_1127202546" MODIFIED="1528160514861" TEXT="Clear COC Enforcement policy">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156860054" ID="ID_509897772" MODIFIED="1528160514862" TEXT="Spaces w/o alcohol as highlights of the conf (not afterthought)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156874166" ID="ID_1247590542" MODIFIED="1528160514863" TEXT="Visible / recognizable safety staff">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156892796" ID="ID_718338743" MODIFIED="1528160514864" TEXT="Quick / easy conduct report system">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156901166" ID="ID_844114731" MODIFIED="1528160514865" TEXT="Public conduct report system (to ensure org accountability)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156909066" ID="ID_1978419417" MODIFIED="1528160514866" TEXT="Include safety information with COC (police, local authorities, etc.)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156921039" ID="ID_684800410" MODIFIED="1528160514867" TEXT="Anonymous &quot;concern report&quot; to surface potential issues with speakers or organizers">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156595147" ID="ID_548615010" MODIFIED="1528160514868" POSITION="right" TEXT="Women">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528156776348" ID="ID_76234299" MODIFIED="1528160514869" TEXT="Submit talks">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528156962546" ID="ID_1235971215" MODIFIED="1528160514870" TEXT="Reach out and invite them directly">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156979132" ID="ID_516236265" MODIFIED="1528160514870" TEXT="Invite them for their expertise">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156986413" ID="ID_1243301743" MODIFIED="1528160514871" TEXT="Reach out with suggested topics based on expertise">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528156998036" ID="ID_1747186430" MODIFIED="1528160514872" TEXT="Don&apos;t invite them to a WIT panel if you haven&apos;t invited them for another reason too">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157032289" ID="ID_1540838774" MODIFIED="1528160514879" TEXT="Don&apos;t ask them to be a &quot;woman speaker&quot; or say your conference needs more diversity (that&apos;s not why you&apos;re inviting them, even if it&apos;s true)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156596793" ID="ID_1507446464" MODIFIED="1528160514883" POSITION="right" TEXT="POC">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528156780574" ID="ID_834678797" MODIFIED="1528160514884" TEXT="Submit talks">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528157077210" ID="ID_207194625" MODIFIED="1528160514884" TEXT="Invite them directly for their expertise">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157086251" ID="ID_17843365" MODIFIED="1528160514885" TEXT="Reach out with suggested topics">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157090270" ID="ID_765063775" MODIFIED="1528160514885" TEXT="Don&apos;t say you need more POC speakers (even if you do; that&apos;s why you&apos;re not inviting them)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156598032" ID="ID_1859391103" MODIFIED="1528160514886" POSITION="right" TEXT="Moms">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528156783459" ID="ID_872675941" MODIFIED="1528160514886" TEXT="Submit talks">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528157244028" ID="ID_1895139150" MODIFIED="1528160514887" TEXT="Make a stocked mothers&apos; room available &amp; close to conf space">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157254534" ID="ID_1603474960" MODIFIED="1528160514887" TEXT="Provide free or subsidized child care services">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157269915" ID="ID_1237347466" MODIFIED="1528160514888" TEXT="Subsidize family / child travel costs">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156599309" ID="ID_897793494" MODIFIED="1528160514888" POSITION="right" TEXT="UR speakers with financial barriers">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528156786198" ID="ID_1000317860" MODIFIED="1528160514889" TEXT="Submit talks">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528157324765" ID="ID_300907669" MODIFIED="1528160514890" TEXT="Cover travel costs">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157335203" ID="ID_1679569322" MODIFIED="1528160514890" TEXT="Offer a stipend to those whose companies don&apos;t support traveling to speak">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157356423" ID="ID_596391805" MODIFIED="1528160514897" TEXT="Provide cost coverage for 1 additional person of speaker&apos;s choice -- so someone speaking also provides an opportunity to others">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157398816" ID="ID_480954247" MODIFIED="1528160514900" TEXT="Give free advertising to employers who cover UR speaker costs (shout-outs, tweets, etc.)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157430447" ID="ID_915206991" MODIFIED="1528160514901" TEXT="Scholarship for UR attendees (more diverse attendees is more attractive to diverse speakers)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156626832" ID="ID_735690817" MODIFIED="1528160514901" POSITION="right" TEXT="LGBTQIA+ speakers">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528156789401" ID="ID_1457681230" MODIFIED="1528160514902" TEXT="Submit talks">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528157485125" ID="ID_563702270" MODIFIED="1528160514902" TEXT="Gender neutral bathrooms">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157489827" ID="ID_285360715" MODIFIED="1528160514902" TEXT="Inclusive &amp; welcoming language everywhere">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157498430" ID="ID_33164637" MODIFIED="1528160514903" TEXT="Free-form / decline option for any gender demographics / language">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156637319" ID="ID_1648337584" MODIFIED="1528160514903" POSITION="right" TEXT="Speakers w/disabilities">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528156791706" ID="ID_1490934505" MODIFIED="1528160514903" TEXT="Submit talks">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528157530184" ID="ID_775291739" MODIFIED="1528160514904" TEXT="Draft an accessibility statement">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157540776" ID="ID_1705337062" MODIFIED="1528160514904" TEXT="Set expectations clearly on site (length of sessions, building accessibility)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157567162" ID="ID_1969021219" MODIFIED="1528160514904" TEXT="Ensure venue has appropriate access &amp; advertise it">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156646023" ID="ID_247002191" MODIFIED="1528160514904" POSITION="right" TEXT="UR speakers with time barriers">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528156796161" ID="ID_266866309" MODIFIED="1528160514905" TEXT="Submit talks">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528157592320" ID="ID_799348488" MODIFIED="1528160514905" TEXT="Publish conference info as early as possible">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157600295" ID="ID_144707637" MODIFIED="1528160514905" TEXT="Ensure a long CFP time (to reach out, gain interest, and let people craft submissions)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156654843" ID="ID_487507180" MODIFIED="1528160514905" POSITION="right" TEXT="Overrepresented Speakers">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528157638628" ID="ID_1634445304" MODIFIED="1528160514908" TEXT="Amplify conferences looking for UR speakers">
+<edge COLOR="#00b439" WIDTH="2"/>
+</node>
+<node COLOR="#00b439" CREATED="1528157649830" ID="ID_1128297749" MODIFIED="1528160514909" TEXT="Tie your participation to others&apos; inclusion">
+<edge COLOR="#00b439" WIDTH="2"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156661320" ID="ID_1167575746" MODIFIED="1528160514909" POSITION="right" TEXT="UR speakers w/imposter syndrome">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528156804530" ID="ID_414418757" MODIFIED="1528160514909" TEXT="Submit talks">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528157698632" ID="ID_684405896" MODIFIED="1528160514909" TEXT="Nomination forms to suggest a talk by a UR speaker">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157709727" ID="ID_1428374855" MODIFIED="1528160514909" TEXT="Make space for first-time speakers">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157718090" ID="ID_1019401508" MODIFIED="1528160514910" TEXT="Offer coaching sessions / mentorship office hours to help craft submissions">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157731212" ID="ID_1692447312" MODIFIED="1528160514910" TEXT="Make the CFP process easy to navigate. Clear language, link to help if you get stuck, etc.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157754720" ID="ID_1260913686" MODIFIED="1528160514910" TEXT="Suggest topics (with input from UR communities)">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157763587" ID="ID_779887155" MODIFIED="1528160514910" TEXT="List past selected topics">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157770422" ID="ID_1100877438" MODIFIED="1528160514911" TEXT="Showcase accepted proposals to show what you look for">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528157778794" ID="ID_1798851186" MODIFIED="1528160514911" TEXT="Link to encouraging sites, e.g. WAAA">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156669926" ID="ID_1608026140" MODIFIED="1528160514911" POSITION="right" TEXT="Conference organizers">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528159963846" ID="ID_1483555263" MODIFIED="1528160514911" TEXT="More direct outreach">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528159983195" ID="ID_722124375" MODIFIED="1528160514911" TEXT="Organizer team members dedicated to outreach">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528159997005" ID="ID_914777438" MODIFIED="1528160514912" TEXT="Partner with local underrepresented groups">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160019851" ID="ID_92448887" MODIFIED="1528160514912" TEXT="Ask prominent people in underrepresented communities to amplify">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160227384" ID="ID_803813796" MODIFIED="1528160514912" TEXT="Diverse conference organizers">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1528159969633" ID="ID_1098732154" MODIFIED="1528160514913" TEXT="Understand underrepresented communities">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528160040685" ID="ID_791992592" MODIFIED="1528160514913" TEXT="Diverse organizer teams">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160047524" ID="ID_777809751" MODIFIED="1528160514913" TEXT="Invite / pay consultants from underrepresented groups">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1528159976533" ID="ID_1457879738" MODIFIED="1528160514913" TEXT="Make diversity &amp; representation a stated goal of the conference">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528160079061" ID="ID_1006769627" MODIFIED="1528160514913" TEXT="Text on the home page">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160246027" ID="ID_1347196797" MODIFIED="1528160514914" TEXT="Diverse conference organizers">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160081958" ID="ID_548638286" MODIFIED="1528160514914" TEXT="Text on CFP">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160083885" ID="ID_865263141" MODIFIED="1528160514914" TEXT="Mention it along with public tweets, etc.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160089390" ID="ID_1664180087" MODIFIED="1528160514914" TEXT="Space dedicated to all the things you&apos;re offering for UR speakers">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160099569" ID="ID_1502975403" MODIFIED="1528160514915" TEXT="Mention an anonymous selection process">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1528160116768" ID="ID_1065053229" MODIFIED="1528160514915" TEXT="Understand why people don&apos;t submit">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528160122353" ID="ID_1261418250" MODIFIED="1528160514915" TEXT="Submission form -- if someone doesn&apos;t submit, ask them why">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160252067" ID="ID_678245201" MODIFIED="1528160514915" TEXT="Diverse conference organizers">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160129049" ID="ID_1727024993" MODIFIED="1528160514916" TEXT="On the space dedicated to &quot;diversity&quot;, ask &quot;if this isn&apos;t enough, let us know and we&apos;ll fix it.&quot;">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1528156674652" ID="ID_1242587237" MODIFIED="1528160514916" POSITION="right" TEXT="Advertisers">
+<edge COLOR="#0033ff" WIDTH="4"/>
+</node>
+<node COLOR="#0033ff" CREATED="1528156678277" ID="ID_434606235" MODIFIED="1528160514916" POSITION="right" TEXT="Selection committees">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1528160176519" ID="ID_1373655669" MODIFIED="1528160514916" TEXT="Choose more underrepresented speakers">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1528160184347" ID="ID_225402303" MODIFIED="1528160514917" TEXT="&quot;blind&quot; submission process">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160195408" ID="ID_1951108919" MODIFIED="1528160514917" TEXT="decide in advance to do more outreach if speakers are too homogenous">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1528160210328" ID="ID_1827895184" MODIFIED="1528160514917" TEXT="Ensure the selection committee is diverse">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+</node>
+</map>
