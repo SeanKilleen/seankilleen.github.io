f8d41ae7932d2fed6b342f0e6dd271cc47579076
Sean Killeen <SeanKilleen@gmail.com>
2022-05-18 07:36:12 -0500

    update workspace location

--- .devcontainer/aliases.sh
+++ .devcontainer/aliases.sh
@@ -2,7 +2,7 @@
 # We get the first workspace, because there should only be one
 # This is so that when someone updates their devcontainer name, it doesn't get screwed up
 first_workspace="$(cd /workspaces && ls | head -1)"
-alias gotoworkspace="cd /workspaces/$first_workspace/docs"
+alias gotoworkspace="cd /workspaces/$first_workspace"
 alias localdev="gotoworkspace && bundle install && bundle exec jekyll serve --future --force_polling --livereload"
 alias build="gotoworkspace && bundle exec jekyll build"
 alias serve="gotoworkspace && bundle exec jekyll serve --future --force_polling --livereload"
