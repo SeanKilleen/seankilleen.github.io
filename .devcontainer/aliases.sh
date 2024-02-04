32646d62edc12b25b148869ca3868d8e2635f175
Sean Killeen <SeanKilleen@gmail.com>
2024-02-04 10:27:35 -0500

    Fix up devcontainer aliases

--- .devcontainer/aliases.sh
+++ .devcontainer/aliases.sh
@@ -3,8 +3,9 @@
 # This is so that when someone updates their devcontainer name, it doesn't get screwed up
 first_workspace="$(cd /workspaces && ls | head -1)"
 alias gotoworkspace="cd /workspaces/$first_workspace"
-alias localdev="gotoworkspace && bundle install && bundle exec jekyll serve --future --force_polling --livereload"
+alias localdev="gotoworkspace && bundle exec jekyll serve --future --force_polling --livereload"
 alias build="gotoworkspace && bundle exec jekyll build"
 alias serve="gotoworkspace && bundle exec jekyll serve --future --force_polling --livereload"
 alias spellcheck="gotoworkspace && cspell --no-progress --config cSpell.json **/*.md"
-alias lint="gotoworkspace && markdownlint -c .markdownlintrc **/*.md"
+alias lint="gotoworkspace && markdownlint-cli2 --config .markdownlint-cli2.jsonc **/*.md"
+alias lintfix="gotoworkspace && markdownlint-cli2 --fix --config .markdownlint-cli2.jsonc **/*.md"
